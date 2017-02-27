5e29e575cba401e65f5c5e069bd563859052a430
bmeritter <bme_ritter@foxmail.com>
2017-02-27 19:00:25 +0800

    csv-demo

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var fs = require('fs');
+var csv = require('csv');
+
+var students = [
+  {
+    name: 'a',
+    age: 1
+  }, {
+    name: 'b',
+    age: 2
+  }, {
+    name: 'c',
+    age: 3
+  }, {
+    name: 'd',
+    age: 4
+  }, {
+    name: 'e',
+    age: 5
+  }
+];
+
+var app = express();
+
+app.get('/students', function (req, res) {
+
+  var content = '';
+
+  content += '姓名,年龄\n';
+  for (var i = 0; i < students.length; ++i) {
+
+    content += students[i].name + ',';
+    content += students[i].age + '\n';
+  }
+
+  var filename = 'test.csv';
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + filename + '');
+  res.setHeader('Content-Type', 'text/csv');
+
+  res.send(content);
+
+});
+
+app.listen(4000);
